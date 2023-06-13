c3b8650762b07d0f5ae7db5fc2006efebf30da9b
Jeff Gonzalez <jeff@hypertheory.com>
2023-06-13 14:29:59 -0400

    minor change

--- backend/BusinessSolution/BusinessApi/Program.cs
+++ backend/BusinessSolution/BusinessApi/Program.cs
@@ -35,7 +35,7 @@ app.MapGet("/clock", ([FromServices] BusinessClock businessClock) =>
 app.MapGet("/info", () =>
 {
     var fullName = Formatters.FormatName("Bob", "Smith");
-    return Results.Ok(new { number = "555-1212", fullName = fullName });
+    return Results.Ok(new { number = "555-1212", fullName = fullName, age=21 });
 });
 // Start the Web Server (Kestrel)
 
